ff03f73e08149f67603fe6943a9240ce42add7e8
zyj5401 <540167691@qq.com>
2018-10-11 14:57:32 +0800

    hitao

---
+++ hitao_system/src/main/java/com/EnterClass.java
@@ -0,0 +1,12 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterClass {
+	public static void main(String[] args) {
+		SpringApplication.run(EnterClass.class, args);
+	}
+
+}
